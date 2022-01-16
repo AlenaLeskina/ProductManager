d20a257546853337f67f5df37955be8bac3cfdfc
Leskin_Andrey <leskinandrei@gmail.com>
2022-01-17 00:09:21 +0300

    product manager

---
+++ src/test/java/ru/netology/domain/ProductTest.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+
+
+    @Test
+    public void shouldUseEquals1() {
+        Product product = new Product();
+    }
+}
